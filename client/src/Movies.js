a3211949fbeba82278a7699b0076396e313b1ba6
evantwidwell <evanski@gmail.com>
2020-12-12 14:48:09 -0600

    removing secrets

--- client/src/Movies.js
+++ client/src/Movies.js
@@ -17,26 +17,6 @@ function Movies() {
     })
   }, []);
 
-  // sendRequest = (title) => {
-  //   const req = unirest(
-  //     "GET",
-  //     "https://imdb8.p.rapidapi.com/title/auto-complete"
-  //   );
-  //   req.query({
-  //     "q": title
-  //   });
-  //   req.headers({
-  //     "x-rapidapi-key": "a608268fc8mshc69aab12951b152p133669jsne7f7964b8f6f",
-  //   	"x-rapidapi-host": "imdb8.p.rapidapi.com",
-  //   	"useQueryString": true
-  //   });
-  //   req.end((res) => {
-  //     if (res.error) throw new Error(res.error);
-  //     console.log(res.body);
-  //     const poster = res.body.i[0].imageUrl;
-  //     this.setPoster({ poster });
-  //   });
-  // }
   const submitReview = () =>{
     Axios.post("http://localhost:3001/api/insert", {
       movieName: movieName,
